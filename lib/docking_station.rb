d64449ce1e2e2e9bc10d1fbdd15adff653f97935
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-15 10:57:16 +0100

    :construction::white_check_mark: Add code to raise error when trying to take bike from empty docking station and pass tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,10 +6,15 @@ class DockingStation
   end
 
   def release_bike
+    raise "The dock is empty" if empty?
     Bike.new
   end
 
   def dock_bike(bike)
     @bikes << bike
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,7 +12,9 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases a working bike" do
-    new_bike = @docking_station.release_bike
+    new_bike = Bike.new
+    @docking_station.dock_bike(new_bike)
+    @docking_station.release_bike
     expect(new_bike).to be_instance_of(Bike)
     expect(new_bike).to be_working
   end
@@ -22,8 +24,12 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
   it "allows a bike to be docked" do
-    bike1 = Bike.new
-    @docking_station.dock_bike(bike1)
-    expect(@docking_station.bikes).to include(bike1)
+    new_bike = Bike.new
+    @docking_station.dock_bike(new_bike)
+    expect(@docking_station.bikes).to include(new_bike)
+  end
+
+  it "should raise error if the dock is empty" do
+    expect { @docking_station.release_bike }.to raise_error("The dock is empty")
   end
 end
