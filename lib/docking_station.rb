296c82b84961926c5c0b1b1271c48d8da0e8e780
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-15 11:18:12 +0100

    :construction::white_check_mark: Add code and methods to check if dock is full and raise error if trying to dock when full plus refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,27 @@
 class DockingStation
+  DEFAULT_CAPACITY = 20
   attr_accessor :bikes
+  attr_reader :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
     raise "The dock is empty" if empty?
-    Bike.new
+    @bikes.pop
   end
 
   def dock_bike(bike)
+    raise "The dock is full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length >= @capacity
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -32,4 +32,15 @@ describe DockingStation do
   it "should raise error if the dock is empty" do
     expect { @docking_station.release_bike }.to raise_error("The dock is empty")
   end
+
+  it "has a default capacity of 20" do
+    test_capacity = rand(1..100)
+    testing_docking_station = DockingStation.new(test_capacity)
+    expect(testing_docking_station.capacity).to eq (test_capacity)
+  end
+
+  it "only allows up to the default capacity of bikes to be docked if capacity not specified" do
+    (@docking_station.capacity).times { @docking_station.bikes << Bike.new }
+    expect { @docking_station.dock_bike(Bike.new) }.to raise_error("The dock is full")
+  end
 end
